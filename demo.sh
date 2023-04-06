2a8c043320d655328f8afc9f5c1708d4d79f0ed9
gkcloudtech777 <ganeshdevops0689@gmail.com>
2023-04-06 09:20:00 +0530

    update demo bash file

--- demo.sh
+++ demo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#echo "Build jenkikns job succesfully"
-echo "Enabled webhook to build job automatically"
-date
+echo "<--------- Display current system uptime information --------->"
+uptime
+
+echo "<--------- Display current mount point information --------->"
+df -h
